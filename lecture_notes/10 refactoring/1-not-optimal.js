ac57ae64c47b3f5c7e37f26a924e3f0def79bd63
Zakhar-Khmurych <68910550+Zakhar-Khmurych@users.noreply.github.com>
2021-12-15 10:14:23 +0200

    Update 1-not-optimal.js
    It is fine

--- lecture_notes/10 refactoring/1-not-optimal.js
+++ lecture_notes/10 refactoring/1-not-optimal.js
@@ -19,9 +19,11 @@ let shift = (offset, points) => {
 const polyline = [
   { x: 0, y: 0 },
   { x: 10, y: 10 },
-  '{ "x": 20, "y": 20 }',
+  { x: 20, y: 20 },
   { x: 30, y: 30 },
 ];
 
 shift({ x: 10, y: -5 }, polyline);
 console.log({ polyline });
+
+
